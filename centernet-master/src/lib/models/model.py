3bacd554c4776a47b80508b0b64339b88441da80
Ross Luo <ross.luo@berkeley.edu>
2019-11-26 13:43:26 -0800

    checkpoint dla dict

--- centernet-master/src/lib/models/model.py
+++ centernet-master/src/lib/models/model.py
@@ -14,6 +14,7 @@ from .networks.pose_dlasigma_dcn import get_pose_net as get_dlasigma_dcn
 from .networks.pose_dlasigmafull_dcn import get_pose_net as get_dlasigmafull_dcn
 from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
 from .networks.large_hourglass import get_large_hourglass_net
+from .networks.checkpoint_dla_dcn import get_pose_net_dcn as get_checkpoint_dla
 
 _model_factory = {
   'res': get_pose_net, # default Resnet with deconv
@@ -23,6 +24,7 @@ _model_factory = {
   'dlasigmafull': get_dlasigmafull_dcn,
   'resdcn': get_pose_net_dcn,
   'hourglass': get_large_hourglass_net,
+  'checkpointdla': get_checkpoint_dla
 }
 
 def create_model(arch, heads, head_conv):
